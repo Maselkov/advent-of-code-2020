d9b67918c5e8d81bd1ce87ab7bd554b4c443ae13
maselkov <Maselkov@users.noreply.github.com>
2020-12-02 17:52:16 +0100

    Tiny optimization

--- d2p2.py
+++ d2p2.py
@@ -4,11 +4,10 @@ valid_count = 0
 with open(f"{path.basename(__file__)[:2]}.txt") as f:
     for line in f.readlines():
         string = line.split(" ")
-        positions = [int(x) - 1 for x in string[0].split("-")]
         letter = string[1][0]
         password = string[2]
         matches = 0
-        for pos in positions:
+        for pos in (int(x) - 1 for x in string[0].split("-")):
             if password[pos] == letter:
                 matches += 1
         if matches == 1:
