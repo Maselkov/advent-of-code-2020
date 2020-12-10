a707992d7f1e5709f7acfe193c84b0d01fc2a526
maselkov <Maselkov@users.noreply.github.com>
2020-12-10 20:16:42 +0100

    Use functools cache

--- d10p2.py
+++ d10p2.py
@@ -1,14 +1,13 @@
 import puzzleinput
+import functools
 
 adapters = sorted(puzzleinput.numbers)
 adapters.insert(0, 0)
 adapters.reverse()
-cache = {}
 
 
+@functools.cache
 def get_possibility_count(node):
-    if node in cache:
-        return cache[node]
     possibilities = 0
     for adapter in adapters:
         difference = node - adapter
@@ -16,7 +15,6 @@ def get_possibility_count(node):
             if adapter == 0:
                 possibilities += 1
             possibilities += get_possibility_count(adapter)
-    cache[node] = possibilities
     return possibilities
 
 
